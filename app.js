d6f8129897ba012dd5595f6c0f542c9bc4f67b5f
Sara Morais <saracristinasm@gmail.com>
2018-08-30 16:21:45 -0400

    adding sequelize

--- app.js
+++ app.js
@@ -24,28 +24,6 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(express.static('client/public'));
 
 
-/* HEROKU DATABASE */
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
-
-app.get('/db', async (req, res) => {
-  try {
-    const client = await pool.connect()
-    const result = await client.query('SELECT * FROM test_table');
-    res.render('pages/db', result);
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-});
-
-
-
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:5000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
